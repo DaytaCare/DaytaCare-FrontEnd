ba0b4a99369421c1e710044d1d706ab2c486719f
Ian Whitmore <ianwhitmor@gmail.com>
2021-12-13 12:07:49 -0600

    finish useFetch hook

--- src/Components/Hooks/useFetch.js
+++ src/Components/Hooks/useFetch.js
@@ -1,8 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
+
 
 export default function useFetch(url) {
 
   const [loading, setLoading] = useState(true);
   const [shouldFetch, setShouldFetch] = useState(true);
   const [tasks, setTasks] = useState(null);
+
+  useEffect(() => {
+    if (!shouldFetch) return;
+
+    async function fetchData() {
+      
+      let response = await fetch(url);
+      let body = await response.json();
+
+      setTasks(body);
+      setLoading(false);
+    };
+
+    setShouldFetch(false);
+    fetchData();
+  }, [url, shouldFetch])
+
+  return useMemo(() => ({
+    tasks,
+    isLoading: loading,
+    setTasks,
+    reload: () => setShouldFetch(true),
+  }), [tasks, loading]);
 }
