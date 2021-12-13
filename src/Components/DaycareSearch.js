2a0ef7fb77fa3fb718f5f38642eb7a80f3f665ed
Ian Whitmore <ianwhitmor@gmail.com>
2021-12-13 12:24:37 -0600

    add spinner if we dont connect to daycare API

--- src/Components/DaycareSearch.js
+++ src/Components/DaycareSearch.js
@@ -1,4 +1,24 @@
 import React from "react";
+import { Spinner } from 'react-bootstrap';
 import useFetch from '../Components/Hooks/useFetch';
 
 const daytaCareApi = 'https://daytacare.azurewebsites.net/api/parents/search';
+
+function DaycareSearch() {
+  const { tasks, reload, setTasks } = useFetch(daytaCareApi);
+
+  if (!daytaCareApi) {
+    return (<Spinner animation="grow" variant="danger" />);
+  }
+  
+
+  return (
+    <div>
+      {daytaCareApi.map(daycare => (
+        <ul>
+          <li></li>
+        </ul>
+      ))}
+    </div>
+  )
+}
