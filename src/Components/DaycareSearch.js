bae1694e5f83f7b078cfa5ba90ad4254eb27ac75
Ian Whitmore <ianwhitmor@gmail.com>
2021-12-13 15:22:19 -0600

    Create DaycareCard Component

--- src/Components/DaycareSearch.js
+++ src/Components/DaycareSearch.js
@@ -5,18 +5,18 @@ import useFetch from '../Components/Hooks/useFetch';
 const daytaCareApi = 'https://daytacare.azurewebsites.net/api/parents/search';
 
 function DaycareSearch() {
-  const { dayCares } = useFetch(daytaCareApi);
+  const { daycares } = useFetch(daytaCareApi);
 
-  if (!dayCares) {
+  if (!daycares) {
     return (<Spinner animation="grow" variant="danger" />);
   }
 
 return (
-  <div>
-    {dayCares.map(dayCare => (
-      <li key={dayCare.id}>{dayCare.name}</li>
+  <>
+    {daycares.map(daycare => (
+      <DaycareCard />
     ))}
-  </div>
+  </>
 )
 }
 
