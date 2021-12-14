62f12f989b8b2e77afc4fdf1d90f071ae79086b0
Ian Whitmore <ianwhitmor@gmail.com>
2021-12-14 12:03:37 -0600

    Search form is now working on submit

--- src/Components/DaycareSearch.js
+++ src/Components/DaycareSearch.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Spinner, Container, Row, Col, Form, Button, Card } from 'react-bootstrap';
+import { Spinner, Row, Col, Form, Button, Card } from 'react-bootstrap';
 import DaycareCards from './DaycareCards';
 import useFetch from '../Components/Hooks/useFetch';
 
@@ -14,71 +14,91 @@ function DaycareSearch() {
     return (<Spinner animation="grow" variant="danger" />);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const { city, state, availability, amenityId } = form.elements;
+    const formData = {
+      city: city.value,
+      state: state.value,
+      availability: availability.checked,
+      amenityId: 0|amenityId.value
+    };
+
+    setParams(formData);
+
+    console.log(formData);
+
+    //form.reset();
+    city.focus();
+  }
+
+
   return (
     <>
       <Card className="mt-4" style={{ width: '40rem' }}>
         <Card.Body>
           <Card.Title className="mb-4">Search Daycares</Card.Title>
-
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group as={Row} className="mb-3" controlId="formHorizontalCity">
               <Form.Label column sm={2}>City</Form.Label>
               <Col sm={10}>
-                <Form.Control type="text" placeholder="City" />
+                <Form.Control type="text" placeholder="City" name="city" />
               </Col>
             </Form.Group>
-
             <Form.Group as={Row} className="mb-3" controlId="formHorizontalState">
               <Form.Label column sm={2}>State</Form.Label>
               <Col sm={10}>
-                <Form.Control type="text" placeholder="State" />
+                <Form.Control type="text" placeholder="State" name="state" />
               </Col>
             </Form.Group>
-
             <Form.Group as={Row} className="mb-3">
               <Form.Label as="legend" column sm={2}>Has Availability</Form.Label>
               <Col sm={10}>
                 <Form.Check
                   type="checkbox"
-                  name="formHorizontalCheckBox" />
+                  name="availability"
+                  value="true" />
               </Col>
             </Form.Group>
-
             <Form.Group as={Row} className="mb-3">
               <Form.Label as="legend" column sm={2}>Amenities</Form.Label>
               <Col sm={10}>
                 <Form.Check
                   type="radio"
                   label="Parking"
-                  name="formHorizontalCheckBox" />
+                  name="amenityId" 
+                  value="1" />
                 <Form.Check
                   type="radio"
                   label="Indoor Playground"
-                  name="formHorizontalCheckBox" />
+                  name="amenityId"
+                  value="2" />
                 <Form.Check
                   type="radio"
                   label="Pay Scaling"
-                  name="formHorizontalCheckBox" />
+                  name="amenityId" />
                 <Form.Check
                   type="radio"
                   label="Shuttle Transportation"
-                  name="formHorizontalCheckBox" />
+                  name="amenityId" />
                 <Form.Check
                   type="radio"
                   label="Security"
-                  name="formHorizontalCheckBox" />
+                  name="amenityId" />
                 <Form.Check
                   type="radio"
                   label="Wheelchair Accessible"
-                  name="formHorizontalCheckBox" />
+                  name="amenityId" />
                 <Form.Check
                   type="radio"
                   label="Education"
-                  name="formHorizontalCheckBox" />
+                  name="amenityId" />
                 <Form.Check
                   type="radio"
                   label="Meal Plan"
-                  name="formHorizontalCheckBox" />
+                  name="amenityId" />
               </Col>
             </Form.Group>
             <Button variant="secondary" type="submit">Submit</Button>
@@ -86,10 +106,9 @@ function DaycareSearch() {
         </Card.Body>
       </Card>
 
-
       <Row>
         {daycares.map(daycare => (
-          <Col>
+          <Col key={daycare.daycareId}>
             <DaycareCards daycare={daycare} />
           </Col>
         ))}
