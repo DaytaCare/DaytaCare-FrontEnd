645fee1b423b66b266feb904c59f960845ee4751
mistidinzy <mistidinzy@gmail.com>
2021-12-14 17:22:51 -0600

    responsive cards

--- src/Components/Home/FeaturedDaycare/FeaturedDaycare.js
+++ src/Components/Home/FeaturedDaycare/FeaturedDaycare.js
@@ -1,45 +1,42 @@
 import './FeaturedDaycare.css'
-import {Row, Col, Card, Button, ListGroup, ListGroupItem} from 'react-bootstrap'
+import {Card, Button, ListGroup, ListGroupItem, CardGroup} from 'react-bootstrap'
 
 function FeaturedDaycare() {
 
   return(
     <>
     <div className="featuredDaycares">
-      <Row xs='auto' md='auto' lg='auto' className="g-4">
-          <Col>
-            <Card style={{ width: '260px' }}>
-              <Card.Header className="card-header">Teddy Bear Child Care Center, Inc.</Card.Header>
-              <Card.Img variant="top" src="https://via.placeholder.com/250x250" />
-                <Card.Body>
-                  <Card.Text>
-                    <p>Daycare Description</p>
-                    <ul>
-                      <li>Amenity</li>
-                      <li>Amenity</li>
-                      <li>Amenity</li>
-                    </ul>
-                  </Card.Text>
-                </Card.Body>
-                  <ListGroup className="list-group-flush">
-                    <ListGroupItem>Daycare Type: Center</ListGroupItem>
-                    <ListGroupItem>Availability: Yes</ListGroupItem>
-                    <ListGroupItem>(319)365-6534</ListGroupItem>
-                  </ListGroup>
-                <Card.Body>
-                  <Button href="#">Website</Button>
-                </Card.Body>
-            </Card>
-          </Col>
+      <CardGroup>
+        <Card>
+          <Card.Header className="card-header">Teddy Bear Child Care Center, Inc.</Card.Header>
+          <Card.Img variant="top" src="https://via.placeholder.com/250x250" />
+            <Card.Body>
+              <Card.Text>
+                <p>Daycare Description</p>
+                <ul className="amenities">
+                  <li>Amenity</li>
+                  <li>Amenity</li>
+                  <li>Amenity</li>
+                </ul>
+              </Card.Text>
+            </Card.Body>
+              <ListGroup className="list-group-flush">
+                <ListGroupItem>Daycare Type: Center</ListGroupItem>
+                <ListGroupItem>Availability: Yes</ListGroupItem>
+                <ListGroupItem>(319)365-6534</ListGroupItem>
+              </ListGroup>
+            <Card.Body>
+              <Button href="#">Website</Button>
+            </Card.Body>
+        </Card>
 
-          <Col>
-            <Card style={{ width: '260px' }}>
+            <Card>
               <Card.Header className="card-header">Alice's Rainbow Child Care Center</Card.Header>
               <Card.Img variant="top" src="https://via.placeholder.com/250x250" />
                 <Card.Body>
                   <Card.Text>
                     <p>Daycare Description</p>
-                    <ul>
+                    <ul className="amenities">
                       <li>Amenity</li>
                       <li>Amenity</li>
                       <li>Amenity</li>
@@ -55,16 +52,14 @@ function FeaturedDaycare() {
                   <Button href="#">Website</Button>
                 </Card.Body>
             </Card>
-          </Col>
 
-          <Col>
-            <Card style={{ width: '260px' }}>
+            <Card>
               <Card.Header className="card-header">KidsPoint Downtown Learning Center & Preschool</Card.Header>
               <Card.Img variant="top" src="https://via.placeholder.com/250x250" />
                 <Card.Body>
                   <Card.Text>
                     <p>Daycare Description</p>
-                    <ul>
+                    <ul className="amenities">
                       <li>Amenity</li>
                       <li>Amenity</li>
                       <li>Amenity</li>
@@ -80,8 +75,7 @@ function FeaturedDaycare() {
                   <Button href="#">Website</Button>
                 </Card.Body>
             </Card>
-          </Col>
-      </Row>
+            </CardGroup>
     </div>
     </>
   )
