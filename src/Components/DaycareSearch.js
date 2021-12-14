3f8becad2c4c5aa279bac15fc1d29d5956adce47
Ian Whitmore <ianwhitmor@gmail.com>
2021-12-14 11:07:17 -0600

    Form now wrapped in a card for better formatting

--- src/Components/DaycareSearch.js
+++ src/Components/DaycareSearch.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Spinner, Container, Row, Col, Form } from 'react-bootstrap';
+import { Spinner, Container, Row, Col, Form, Button, Card } from 'react-bootstrap';
 import DaycareCards from './DaycareCards';
 import useFetch from '../Components/Hooks/useFetch';
 
@@ -15,80 +15,77 @@ function DaycareSearch() {
   }
 
   return (
-    <Container>
-      <Form>
-        <Form.Group as={Row} className="mb-3" controlId="formHorizontalCity">
-          <Form.Label column sm={2}>City</Form.Label>
-          <Col sm={10}>
-            <Form.Control type="text" placeholder="City" />
-          </Col>
-        </Form.Group>
+    <>
+      <Card className="mt-4">
+        <Card.Body>
+          <Card.Title className="mb-4">Search Daycares</Card.Title>
 
-        <Form.Group as={Row} className="mb-3" controlId="formHorizontalState">
-          <Form.Label column sm={2}>State</Form.Label>
-          <Col sm={10}>
-            <Form.Control type="text" placeholder="State" />
-          </Col>
-        </Form.Group>
+          <Form>
+            <Form.Group as={Row} className="mb-3" controlId="formHorizontalCity">
+              <Form.Label column sm={2}>City</Form.Label>
+              <Col sm={10}>
+                <Form.Control type="text" placeholder="City" />
+              </Col>
+            </Form.Group>
 
-        <fieldset>
-          <Form.Group as={Row} className="mb-3">
-            <Form.Label as="legend" column sm={2}>Availability Y/N</Form.Label>
-            <Col sm={10}>
-              <Form.Check
-                type="checkbox"
-                name="formHorizontalCheckBox"
-              />
-            </Col>
-          </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formHorizontalState">
+              <Form.Label column sm={2}>State</Form.Label>
+              <Col sm={10}>
+                <Form.Control type="text" placeholder="State" />
+              </Col>
+            </Form.Group>
 
-          <Form.Group as={Row} className="mb-3">
-            <Form.Label as="legend" column sm={2}>Amenities</Form.Label>
-            <Col sm={10}>
-              <Form.Check
-                type="checkbox"
-                label="Parking"
-                name="formHorizontalCheckBox"
-              />
-              <Form.Check
-                type="checkbox"
-                label="Indoor Playground"
-                name="formHorizontalCheckBox"
-              />
-              <Form.Check
-                type="checkbox"
-                label="Pay Scaling"
-                name="formHorizontalCheckBox"
-              />
-              <Form.Check
-                type="checkbox"
-                label="Shuttle Transportation"
-                name="formHorizontalCheckBox"
-              />
-              <Form.Check
-                type="checkbox"
-                label="Security"
-                name="formHorizontalCheckBox"
-              />
-              <Form.Check
-                type="checkbox"
-                label="Wheelchair Accessible"
-                name="formHorizontalCheckBox"
-              />
-              <Form.Check
-                type="checkbox"
-                label="Education"
-                name="formHorizontalCheckBox"
-              />
-              <Form.Check
-                type="checkbox"
-                label="Meal Plan"
-                name="formHorizontalCheckBox"
-              />
-            </Col>
-          </Form.Group>
-        </fieldset>
-      </Form>
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label as="legend" column sm={2}>Has Availability</Form.Label>
+              <Col sm={10}>
+                <Form.Check
+                  type="checkbox"
+                  name="formHorizontalCheckBox" />
+              </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="mb-3">
+              <Form.Label as="legend" column sm={2}>Amenities</Form.Label>
+              <Col sm={10}>
+                <Form.Check
+                  type="radio"
+                  label="Parking"
+                  name="formHorizontalCheckBox" />
+                <Form.Check
+                  type="radio"
+                  label="Indoor Playground"
+                  name="formHorizontalCheckBox" />
+                <Form.Check
+                  type="radio"
+                  label="Pay Scaling"
+                  name="formHorizontalCheckBox" />
+                <Form.Check
+                  type="radio"
+                  label="Shuttle Transportation"
+                  name="formHorizontalCheckBox" />
+                <Form.Check
+                  type="radio"
+                  label="Security"
+                  name="formHorizontalCheckBox" />
+                <Form.Check
+                  type="radio"
+                  label="Wheelchair Accessible"
+                  name="formHorizontalCheckBox" />
+                <Form.Check
+                  type="radio"
+                  label="Education"
+                  name="formHorizontalCheckBox" />
+                <Form.Check
+                  type="radio"
+                  label="Meal Plan"
+                  name="formHorizontalCheckBox" />
+              </Col>
+            </Form.Group>
+            <Button variant="secondary" type="submit">Submit</Button>
+          </Form>
+        </Card.Body>
+      </Card>
+
 
       <Row>
         {daycares.map(daycare => (
@@ -97,7 +94,7 @@ function DaycareSearch() {
           </Col>
         ))}
       </Row>
-    </Container>
+    </>
   )
 }
 
