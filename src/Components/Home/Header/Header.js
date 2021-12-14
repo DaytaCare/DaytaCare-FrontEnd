957597765102a5d00cad0e62977245a727558b57
mistidinzy <mistidinzy@gmail.com>
2021-12-14 16:55:55 -0600

    added logo image to home page and added lorem(cupcake) ipsum to about section

--- src/Components/Home/Header/Header.js
+++ src/Components/Home/Header/Header.js
@@ -8,7 +8,7 @@ function Header() {
 
   return (
     <>
-      <Navbar bg="light" variant="light" className="navbar">
+      <Navbar className="navbar">
         <Navbar.Brand href="#" className="navbar-brand">
           <img
             alt=""
@@ -20,23 +20,19 @@ function Header() {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-
-
-          <Nav className="me-auto">
+          <Nav variant="tabs" className="me-auto">
             <Nav.Link as={Link} to="/" className="navbar-link">Home</Nav.Link>
             <Nav.Link as={Link} to="/AboutUs" className="navbar-link" >About Us</Nav.Link>
             <Nav.Link as={Link} to="/DaycareSearch" className="navbar-link" >Find a Daycare</Nav.Link>
             <Nav.Link as={Link} to="/DaycareDashboard">Daycare Dashboard</Nav.Link>
           </Nav>
-
           <NavDropdown title="Log In" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="/Login">Login</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
             <NavDropdown.Divider />
             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
           </NavDropdown>
-
           {!user && <Nav.Link as={Link} to='/Login' className="login-link" >Login</Nav.Link>}
           {user &&
             <>
@@ -44,9 +40,8 @@ function Header() {
               <button onClick={() => logout()}>Log Out</button>
             </>
           }
-
-        </Navbar.Collapse>
-      </Navbar>
+          </Navbar.Collapse>
+        </Navbar>
     </>
   )
 }
