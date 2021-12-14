33cf822797c70168e9f8b1d1b8ec2880d602676a
Ian Whitmore <ianwhitmor@gmail.com>
2021-12-14 10:36:58 -0600

    Form now has all appropriate fields

--- src/Components/DaycareSearch.js
+++ src/Components/DaycareSearch.js
@@ -6,7 +6,7 @@ import useFetch from '../Components/Hooks/useFetch';
 const daytaCareApi = 'https://daytacare.azurewebsites.net/api/parents/search';
 
 function DaycareSearch() {
-  
+
   const [params, setParams] = useState({});
   const { daycares } = useFetch(daytaCareApi, params);
 
@@ -17,14 +17,79 @@ function DaycareSearch() {
   return (
     <Container>
       <Form>
-        <Form.Group>
-          <Form.Label>City</Form.Label>
-          <Form.Control type="text" placeholder="City" />
-          <Form.Label>State</Form.Label>
-          <Form.Control type="text" placeholder="State" />
-          
+        <Form.Group as={Row} className="mb-3" controlId="formHorizontalCity">
+          <Form.Label column sm={2}>City</Form.Label>
+          <Col sm={10}>
+            <Form.Control type="text" placeholder="City" />
+          </Col>
         </Form.Group>
+
+        <Form.Group as={Row} className="mb-3" controlId="formHorizontalState">
+          <Form.Label column sm={2}>State</Form.Label>
+          <Col sm={10}>
+            <Form.Control type="text" placeholder="State" />
+          </Col>
+        </Form.Group>
+
+        <fieldset>
+          <Form.Group as={Row} className="mb-3">
+            <Form.Label as="legend" column sm={2}>Availability Y/N</Form.Label>
+            <Col sm={10}>
+              <Form.Check
+                type="checkbox"
+                name="formHorizontalCheckBox"
+              />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} className="mb-3">
+            <Form.Label as="legend" column sm={2}>Amenities</Form.Label>
+            <Col sm={10}>
+              <Form.Check
+                type="checkbox"
+                label="Parking"
+                name="formHorizontalCheckBox"
+              />
+              <Form.Check
+                type="checkbox"
+                label="Indoor Playground"
+                name="formHorizontalCheckBox"
+              />
+              <Form.Check
+                type="checkbox"
+                label="Pay Scaling"
+                name="formHorizontalCheckBox"
+              />
+              <Form.Check
+                type="checkbox"
+                label="Shuttle Transportation"
+                name="formHorizontalCheckBox"
+              />
+              <Form.Check
+                type="checkbox"
+                label="Security"
+                name="formHorizontalCheckBox"
+              />
+              <Form.Check
+                type="checkbox"
+                label="Wheelchair Accessible"
+                name="formHorizontalCheckBox"
+              />
+              <Form.Check
+                type="checkbox"
+                label="Education"
+                name="formHorizontalCheckBox"
+              />
+              <Form.Check
+                type="checkbox"
+                label="Meal Plan"
+                name="formHorizontalCheckBox"
+              />
+            </Col>
+          </Form.Group>
+        </fieldset>
       </Form>
+
       <Row>
         {daycares.map(daycare => (
           <Col>
