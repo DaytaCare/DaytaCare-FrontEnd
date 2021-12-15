5192ca9966778b9d9ccec39c52162f7fd93b82d1
Ian Whitmore <ianwhitmor@gmail.com>
2021-12-15 12:45:41 -0600

    Add close button to modal, and useState for closing modal

--- src/Components/DaycareDetailModal.js
+++ src/Components/DaycareDetailModal.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { Modal } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+import DaycareSearch from './DaycareSearch';
 import useFetch from './Hooks/useFetch';
+import { useState } from 'react';
 
 function DaycareDetailModal() {
+
+  const [show, setShow] = useState(false);
+  
+  function handleClose() {
+    setShow(false)
+  };
 
   const { id } = useParams();
   const daytaCareApi = `https://daytacare.azurewebsites.net/api/parents/daycare/${id}`;
@@ -14,7 +22,10 @@ function DaycareDetailModal() {
   console.log("daycare:", daycareId);
 
   return (
-    <Modal show={true}>
+    <Modal show={true} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Hi</Modal.Title>
+      </Modal.Header>
 
     </Modal>
   )
