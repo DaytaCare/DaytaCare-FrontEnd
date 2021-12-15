139698bcb05e448e51518a69f4a8f7a63d29a716
ymcla <ymclapp@yahoo.com>
2021-12-15 13:51:04 -0600

    added registration card to use the two forms

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -1,5 +1,28 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Nav, Button } from 'react-bootstrap';
 
+export default function Registration() {
 
-
+    return (
+        <Card>
+            <Card.Header>
+                <Nav variant="tabs" defaultActiveKey="#first">
+                    <Nav.Item>
+                        <Nav.Link href="/ParentForm">Parent Registration</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/DaycareForm">Owner Registration</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Card.Header>
+            <Card.Body>
+                <Card.Title>Registration Form</Card.Title>
+                <Card.Text>
+                    If you are a parent and wanting to search for available daycares, please use the Parent Registration
+                    Daycare owners, please use the Owner Registration to register and add your daycares.
+                </Card.Text>
+                <Button variant="primary">I don't think we need this button unless to cancel/close form</Button>
+            </Card.Body>
+        </Card>
+    )
+}
