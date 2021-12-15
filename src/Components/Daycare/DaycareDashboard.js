4b50b1125d654f3602f2e2adb6c2166ababfe544
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-12-15 13:39:19 -0600

    added props for on save

--- src/Components/Daycare/DaycareDashboard.js
+++ src/Components/Daycare/DaycareDashboard.js
@@ -1,5 +1,6 @@
 import useFetch from '../Hooks/useFetch';
-import { Card, Col, Button, Modal} from 'react-bootstrap';
+import useAuth from '../Hooks/useAuth';
+import { Card, Col, Button, Modal } from 'react-bootstrap';
 import DaycareAdd from '../Daycare/DaycareAdd';
 import { useState } from 'react';
 
@@ -11,39 +12,62 @@ export default function ShowMyDaycares() {
   console.log("daycares", daycares)
 
   const [showAddForm, setShowAddForm] = useState(false);
+  const { reload, isLoading } = useFetch(daytaCareApi);
+  const { user } = useAuth();
 
 
   const handleClose = () => setShowAddForm(false);
-  
 
-  function handleShowAddForm(event){
+
+  function handleShowAddForm(event) {
     event.preventDefault();
 
     setShowAddForm(true);
   }
 
+  async function handleDeleteDaycare(daycare) {
+    if (!user) {
+      console.warn("Anonymous should not be allowed to delete!");
+      return;
+    }
+
+    await fetch(`${daytaCareApi}/${daycare.daycareId}`, {
+      method: 'delete',
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+      }
+    })
+    reload();
+  }
+  if (isLoading) {
+    return (<h2>Loading...</h2>)
+  }
+
 
   return (
     <>
-    <Button onClick={handleShowAddForm}>Add a Daycare</Button>
-    <Modal show={showAddForm} onHide={handleClose}>
-    <Modal.Header closeButton />
-      <DaycareAdd />
+      <Modal show={showAddForm} onHide={handleClose}>
+        <Modal.Header closeButton />
+        <DaycareAdd onSave={handleClose} />
       </Modal>
-    <h1>Your Daycares</h1>
-    {daycares ? daycares.map(
-      (daycare) => (
-        <Col>
-        <Card style={{ width: '300px' }} >
-        <Card.Body>
-        <Card.Title>{daycare.name}</Card.Title>
-        <Card.Text>Type: {daycare.daycareType}</Card.Text>
-        <Card.Text>{daycare.streetAddress}, {daycare.city}</Card.Text>
-        </Card.Body>
-        </Card>
-        </Col>
-          )
-    ) : 'Loading...'}
+      <h1>Your Daycares</h1>
+      <Button onClick={handleShowAddForm}>Add a Daycare</Button>
+      {daycares ? daycares.map(
+        (daycare) => (
+          <Col>
+            <Card style={{ width: '300px' }} >
+              <Card.Body>
+                <Card.Title>{daycare.name}</Card.Title>
+                <Card.Text>Type: {daycare.daycareType}</Card.Text>
+                <Card.Text>{daycare.streetAddress}, {daycare.city}</Card.Text>
+              </Card.Body>
+              <Card.Footer>
+                <Button onClick={() => handleDeleteDaycare(daycare)}>Delete</Button>
+              </Card.Footer>
+            </Card>
+          </Col>
+        )
+      ) : 'Loading...'}
     </>
   );
 }
