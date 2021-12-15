65b85e00a927a5c11733923f02f176d4126281d9
Ian Whitmore <ianwhitmor@gmail.com>
2021-12-15 12:28:07 -0600

    Fix Formatting

--- src/Components/DaycareDetailModal.js
+++ src/Components/DaycareDetailModal.js
@@ -4,18 +4,18 @@ import { useParams } from 'react-router-dom';
 import useFetch from './Hooks/useFetch';
 
 function DaycareDetailModal() {
-    
-  const { id } = useParams();  
+
+  const { id } = useParams();
   const daytaCareApi = `https://daytacare.azurewebsites.net/api/parents/daycare/${id}`;
 
-  const {data: daycareId } = useFetch(daytaCareApi);
-  
+  const { data: daycareId } = useFetch(daytaCareApi);
+
   console.log("id:", id);
   console.log("daycare:", daycareId);
-  
+
   return (
     <Modal show={true}>
-      Test
+
     </Modal>
   )
 }
