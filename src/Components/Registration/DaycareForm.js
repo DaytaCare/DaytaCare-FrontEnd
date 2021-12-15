5ca66ea7667d96789f724aa21a9caecb0cd82e34
ymcla <ymclapp@yahoo.com>
2021-12-15 13:35:35 -0600

    updated daycare registration form to be similar to parent registration form

--- src/Components/Registration/DaycareForm.js
+++ src/Components/Registration/DaycareForm.js
@@ -20,10 +20,20 @@ export default function DaycareRegister() {
     }
 
     return (
-        <Form onSubmit={handleDaycareRegisterSubmit}>
-            <Form.Label>Email:  <input name="email" type="email" /></Form.Label>
-            <Form.Label>Username:  <input name="username" /></Form.Label>
-            <Form.Label> Password:  <input name="password" type="password" /></Form.Label>
+        <Form onSubmit={handleDaycareRegisterSubmit} title="Owner Registration Form">
+            <Form.Group className="mb-3" controlId="form.email">
+                <Form.Label>Email:</Form.Label>
+                <Form.Control type="email" placeholder="jane@example.com" value="email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="form.username">
+                <Form.Label>Username:</Form.Label>
+                <Form.Control type="username" value="username" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="form.password">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control name="password" type="password" value="password" />
+            </Form.Group>
+
             <Button type="submit">Register</Button>
         </Form>
     )
