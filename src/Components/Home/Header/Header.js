c03c84fb9042de62ef86086c178431b4b830f1c3
mistidinzy <mistidinzy@gmail.com>
2021-12-15 14:05:30 -0600

    login form shows up in Nav-Dropdown now

--- src/Components/Home/Header/Header.js
+++ src/Components/Home/Header/Header.js
@@ -1,7 +1,8 @@
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Nav, Navbar, NavItem, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import useAuth from '../../Hooks/useAuth';
 import './Header.css';
+import Login from '../../Auth/Login';
 
 function Header() {
   const { user, logout } = useAuth();
@@ -13,35 +14,43 @@ function Header() {
           <img
             alt=""
             src="/DaytaCare.png"
-            width="70"
-            height="70"
+            width="100"
+            height="100"
           />
           {' '}
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav variant="tabs" className="me-auto">
-            <Nav.Link as={Link} to="/" className="navbar-link">Home</Nav.Link>
-            <Nav.Link as={Link} to="/AboutUs" className="navbar-link" >About Us</Nav.Link>
-            <Nav.Link as={Link} to="/DaycareSearch" className="navbar-link" >Find a Daycare</Nav.Link>
-            <Nav.Link as={Link} to="/DaycareDashboard">Daycare Dashboard</Nav.Link>
+          <Nav className="me-auto">
+            <NavItem className="tab1">
+              <Nav.Link as={Link} to="/" className="navbar-link">Home</Nav.Link>
+            </NavItem>
+
+            <NavItem className="tab2">
+              <Nav.Link as={Link} to="/AboutUs" className="navbar-link">About Us</Nav.Link>
+            </NavItem>
+            <Nav.Link as={Link} to="/DaycareSearch" className="navbar-link">Find a Daycare</Nav.Link>
+
+            <NavItem className="tab2">
+              <Nav.Link as={Link} to="/DaycareDashboard" className="navbar-link">Daycare Dashboard</Nav.Link>
+            </NavItem>
+
+
+            <NavDropdown title="Log In" id="basic-nav-dropdown">
+              <NavDropdown.Item><Login/></NavDropdown.Item>
+            </NavDropdown>
+
+            {!user && <Nav.Link as={Link} to='/Login' className="login-link" >Login</Nav.Link>}
+            {user &&
+              <>
+                Welcome back, {user.username}
+                <button onClick={() => logout()}>Log Out</button>
+              </>
+            }
+
           </Nav>
-          <NavDropdown title="Log In" id="basic-nav-dropdown">
-            <NavDropdown.Item href="/Login">Login</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-          </NavDropdown>
-          {!user && <Nav.Link as={Link} to='/Login' className="login-link" >Log In</Nav.Link>}
-          {user &&
-            <>
-              Welcome back, {user.username}
-              <button onClick={() => logout()}>Log Out</button>
-            </>
-          }
-          </Navbar.Collapse>
-        </Navbar>
+        </Navbar.Collapse>
+      </Navbar>
     </>
   )
 }
