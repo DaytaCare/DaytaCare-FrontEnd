0c690bf5c7a83f4dd0fcbe972c526ca4c5bf8b18
mistidinzy <mistidinzy@gmail.com>
2021-12-15 10:09:52 -0600

    trying to get text on featured daycares to scale with card

--- src/Components/Home/FeaturedDaycare/FeaturedDaycare.js
+++ src/Components/Home/FeaturedDaycare/FeaturedDaycare.js
@@ -7,8 +7,8 @@ function FeaturedDaycare() {
     <>
     <div className="featuredDaycares">
       <CardGroup>
-        <Card>
-          <Card.Header className="card-header1"></Card.Header>
+        <Card className="card">
+          <Card.Header className="card-header1">Featured Daycare</Card.Header>
             <Card.Body>
               <Card.Text>
                 <p>Daycare Description</p>
@@ -28,51 +28,49 @@ function FeaturedDaycare() {
               <Button href="#">Website</Button>
             </Card.Body>
         </Card>
-
-            <Card>
-              <Card.Header className="card-header2">Alice's Rainbow Child Care Center</Card.Header>
-                <Card.Body>
-                  <Card.Text>
-                    <p>Daycare Description</p>
-                    <ul className="amenities">
-                      <li>Amenity</li>
-                      <li>Amenity</li>
-                      <li>Amenity</li>
-                    </ul>
-                  </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    <ListGroupItem>Daycare Type: Center</ListGroupItem>
-                    <ListGroupItem>Availability: Yes</ListGroupItem>
-                    <ListGroupItem>(319)354-1466</ListGroupItem>
-                  </ListGroup>
-                <Card.Body>
-                  <Button href="https://www.arainbowcc.com/">Website</Button>
-                </Card.Body>
-            </Card>
-
-            <Card>
-              <Card.Header className="card-header3">KidsPoint Downtown Learning Center & Preschool</Card.Header>
-                <Card.Body>
-                  <Card.Text>
-                    <p>Daycare Description</p>
-                    <ul className="amenities">
-                      <li>Amenity</li>
-                      <li>Amenity</li>
-                      <li>Amenity</li>
-                    </ul>
-                  </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    <ListGroupItem>Daycare Type: Center</ListGroupItem>
-                    <ListGroupItem>Availability: Yes</ListGroupItem>
-                    <ListGroupItem>(319)555-5555</ListGroupItem>
-                  </ListGroup>
-                <Card.Body>
-                  <Button href="#" className="button">Website</Button>
-                </Card.Body>
-            </Card>
-            </CardGroup>
+        <Card className="card">
+          <Card.Header className="card-header2">Alice's Rainbow Child Care Center</Card.Header>
+            <Card.Body>
+              <Card.Text>
+                <p>Daycare Description</p>
+                <ul className="amenities">
+                  <li>Amenity</li>
+                  <li>Amenity</li>
+                  <li>Amenity</li>
+                </ul>
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroupItem>Daycare Type: Center</ListGroupItem>
+              <ListGroupItem>Availability: Yes</ListGroupItem>
+              <ListGroupItem>(319)354-1466</ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+              <Button href="https://www.arainbowcc.com/">Website</Button>
+            </Card.Body>
+        </Card>
+        <Card className="card">
+          <Card.Header className="card-header3">KidsPoint Downtown Learning Center & Preschool</Card.Header>
+            <Card.Body>
+              <Card.Text>
+                <p>Daycare Description</p>
+                <ul className="amenities">
+                  <li>Amenity</li>
+                  <li>Amenity</li>
+                  <li>Amenity</li>
+                </ul>
+              </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+                <ListGroupItem>Daycare Type: Center</ListGroupItem>
+                <ListGroupItem>Availability: Yes</ListGroupItem>
+                <ListGroupItem>(319)555-5555</ListGroupItem>
+              </ListGroup>
+            <Card.Body>
+              <Button href="#" className="button">Website</Button>
+            </Card.Body>
+        </Card>
+      </CardGroup>
     </div>
     </>
   )
