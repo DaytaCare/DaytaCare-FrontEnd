93ab8da3400f787f03d2a002422ae6c09ee8e53d
Ian Whitmore <ianwhitmor@gmail.com>
2021-12-15 12:03:41 -0600

    Fetching data from daycare by id is now working

--- src/Components/DaycareDetailModal.js
+++ src/Components/DaycareDetailModal.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Modal } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
+import useFetch from './Hooks/useFetch';
 
 function DaycareDetailModal() {
-  
-    let { id } = useParams(); 
     
+  const { id } = useParams();  
+  const daytaCareApi = `https://daytacare.azurewebsites.net/api/parents/daycare/${id}`;
+
+  const {data: daycareId } = useFetch(daytaCareApi);
+  
+  console.log("id:", id);
+  console.log("daycare:", daycareId);
   
   return (
     <Modal show={true}>
