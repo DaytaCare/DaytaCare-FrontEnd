319b1a7a94c965a740f5bdf7c556f0fc2178532e
mistidinzy <mistidinzy@gmail.com>
2021-12-15 23:38:11 -0600

    added styling to daycare provider form

--- src/Components/Registration/DaycareForm.js
+++ src/Components/Registration/DaycareForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import './Registration.css'
 
 const daytaCareApi = 'https://daytacare.azurewebsites.net/api/Users'
@@ -19,53 +19,38 @@ export default function ParentRegister() {
         };
         console.log("Submitting....", daycareRegData);
 
-
         const result = await fetch(`${daytaCareApi}/DaycareRegister`, {
             method: 'post',
             body: JSON.stringify(daycareRegData),
             headers: {
                 'Content-Type': 'application/json',
             },
-
         })
-
         console.log(result);
         form.reset();
         //onSave();
     }
 
     return (
-        <div className="centerD" title="Owner Registration Form">
-            <Form onSubmit={handleDaycareRegisterSubmit} className="formDP">
+        <div className="form-section" title="Owner Registration Form">
+            <Form onSubmit={handleDaycareRegisterSubmit} className="daycare-signup">
                 <legend>Owner Registration Form</legend>
-                <Row>
-                    <Col xs="auto">
-                        <Form.Group className="mb-3" controlId="form.email">
-                            <Form.Label>Email:</Form.Label>
-                            <Form.Control name="email" type="email" placeholder="jane@example.com" />
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="form.username">
-                            <Form.Label>Username:</Form.Label>
-                            <Form.Control name="username" />
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="form.password">
-                            <Form.Label>Password:</Form.Label>
-                            <Form.Control name="password" type="password" />
-                        </Form.Group>
-
-                        <Form.Group>
-                            <Button variant="primary" type="submit" size="lg">Register</Button>
-                        </Form.Group>
-
-                        <Form.Group>
-                            <Button variant="secondary" href="/">Return Home</Button>{' '}
-                            <Button variant="info" href="/Registration">Back</Button>
-                        </Form.Group>
-                    </Col>
-                </Row>
-
+                <Form.Group className="mb-3" controlId="form.email">
+                    <Form.Label>Email:</Form.Label>
+                    <Form.Control name="email" type="email" placeholder="jane@example.com" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="form.username">
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control name="username" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="form.password">
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control name="password" type="password" />
+                </Form.Group>
+                <Form.Group className="form-buttons">
+                    <Button href="/Registration" className="back-button">Back</Button>
+                    <Button type="submit">Register</Button>
+                </Form.Group>
             </Form>
         </div>
     )
--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Nav, Button } from 'react-bootstrap';
+import { Card, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 //import DaycareForm from './DaycareForm';
 //import ParentForm from './ParentForm';
@@ -11,21 +11,13 @@ export default function Registration() {
             <Card.Header>
                 <Nav variant="pills" defaultActiveKey="#first">
                     <Nav.Item>
-                    <Nav.Link as={Link} to="/ParentForm">Parent</Nav.Link>
+                        <Nav.Link as={Link} to="/ParentForm">Parent</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                    <Nav.Link as={Link} to="/DaycareForm">Owner</Nav.Link>
+                        <Nav.Link as={Link} to="/DaycareForm">Daycare Provider</Nav.Link>
                     </Nav.Item>
                 </Nav>
             </Card.Header>
-            <Card.Body>
-                <Card.Title>Registration Form</Card.Title>
-                <Card.Text>
-                    If you are a parent and wanting to search for available daycares, please use the Parent Registration
-                    Daycare owners, please use the Owner Registration to register and add your daycares.
-                </Card.Text>
-                <Button href="/">Cancel</Button>
-            </Card.Body>
         </Card>
     )
 }
