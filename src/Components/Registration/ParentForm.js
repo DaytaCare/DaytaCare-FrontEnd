10a362f4dbc6d76039995f62cf91ebf7faa0acb7
Ian Whitmore <ianwhitmor@gmail.com>
2021-12-17 00:02:27 -0600

    Delete unused Col

--- src/Components/Registration/ParentForm.js
+++ src/Components/Registration/ParentForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Button, Col, Row } from 'react-bootstrap';
+import { Form, Button, Row } from 'react-bootstrap';
 import './ParentForm.css'
 import './Registration.css'
 
