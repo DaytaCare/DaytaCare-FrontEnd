8a64aee2353800a6f404097e25ba46599819de13
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-12-17 11:49:29 -0600

    added featured daycares

--- src/Components/Home/FeaturedDaycare/FeaturedDaycare.js
+++ src/Components/Home/FeaturedDaycare/FeaturedDaycare.js
@@ -1,6 +1,5 @@
 import './FeaturedDaycare.css'
-import { Container, Card, CardGroup } from 'react-bootstrap'
-// import { Button } from 'react-bootstrap'
+import { Container, Card, CardGroup, Button } from 'react-bootstrap'
 
 function FeaturedDaycare() {
   return (
@@ -8,22 +7,46 @@ function FeaturedDaycare() {
       <Container as='div' className="featuredDaycares">
         <CardGroup>
         <Card className="card">
+          <Card.Header>
+          Good Shepherd Center
+          </Card.Header>
             <Card.Img className='featured' variant='top' src="https://bit.ly/3GOmay3"/>
-            {/* <Card.Footer>
-              <Button href="#">Visit</Button>
-            </Card.Footer> */}
+            <Card.Body>
+              <Card.Text>
+                Iowa City, IA<br/>Monthly Price: $975
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <Button href="http://www.gsckids.com/">Visit</Button>
+            </Card.Footer>
           </Card>
           <Card className="card">
+          <Card.Header>
+          Apple Kids
+          </Card.Header>
             <Card.Img className='featured' variant='top' src="https://bit.ly/3dWTbM3" />
-            {/* <Card.Footer>
-              <Button href="#">Visit</Button>
-            </Card.Footer> */}
+            <Card.Body>
+              <Card.Text>
+                Marion, IA<br/>Monthly Price: $950
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <Button href="http://www.applekidsmarion.com/">Visit</Button>
+            </Card.Footer>
           </Card>
           <Card className="card">
+          <Card.Header>
+          KidsPoint C Street
+          </Card.Header>
             <Card.Img className='featured' variant='top' src="https://bit.ly/3yyFcp4" />
-            {/* <Card.Footer>
-              <Button href="#">Visit</Button>
-            </Card.Footer> */}
+            <Card.Body>
+              <Card.Text>
+              Cedar Rapids, IA<br/>Monthly Price: $940
+              </Card.Text>     
+            </Card.Body>
+            <Card.Footer>
+              <Button href="https://www.kidspointchildcare.org/">Visit</Button>
+            </Card.Footer>
           </Card>
         </CardGroup>
       </Container>
