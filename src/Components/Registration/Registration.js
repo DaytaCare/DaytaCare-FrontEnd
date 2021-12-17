09c1161172f6858f721f3d9d0c170d9be5e4f4c4
Jaren Schnoebelen <jarenschnoebelen@yahoo.com>
2021-12-16 23:05:23 -0600

    put the register buttons in cards

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Card, Container } from 'react-bootstrap';
+import './Registration.css'
 // import { Link } from 'react-router-dom';
 //import DaycareForm from './DaycareForm';
 //import ParentForm from './ParentForm';
@@ -8,8 +9,20 @@ export default function Registration() {
 
     return (
         <div className="signupOptions">
-            <Button as="a" href="/ParentForm">Parent</Button>
-            <Button as="a" href="/DaycareForm">Daycare</Button>
+            <Container className="register">
+            <Card className="signupCard" >
+              <Card.Body>
+                <Card.Title>Register as a Parent</Card.Title>
+            <Button className="registerButton" as="a" href="/ParentForm">Parent</Button>
+            </Card.Body>
+            </Card>
+            <Card className="signupCard" >
+              <Card.Body>
+                <Card.Title>Register as a Daycare Owner</Card.Title>
+            <Button className="registerButton" as="a" href="/DaycareForm">Daycare</Button>
+            </Card.Body>
+            </Card>
+            </Container>  
         </div>
 
 
